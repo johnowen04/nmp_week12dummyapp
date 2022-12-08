979b43ee760c40cc58ee8ba2ee88b407c47b5820
John Owen Danuarta <john.owen.d19@gmail.com>
2022-12-09 00:12:49 +0700

    Homework Week 13

--- app/src/main/java/com/ubaya160420016/s160420016_dummyapp/MainActivity.kt
+++ app/src/main/java/com/ubaya160420016/s160420016_dummyapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_SELECT_TEMPLATE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,8 +16,19 @@ class MainActivity : AppCompatActivity() {
             intent.apply {
                 action = Intent.ACTION_VIEW
                 val shareIntent = Intent.createChooser(intent, "Invoke Template")
-                startActivity(shareIntent)
+                startActivityForResult(shareIntent, REQUEST_SELECT_TEMPLATE)
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_SELECT_TEMPLATE -> {
+                    textView.text = data?.getStringExtra("TEMPLATE")
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
